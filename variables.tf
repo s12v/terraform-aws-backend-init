c558c0d46888a7e6dcafd8025d251859cc699a7b
Sergey Novikov <snov@snov.me>
2019-02-05 08:45:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable terraform_state_name {
   type = "string"
   description = "The name of the S3 bucket and DynamoDB table used for Terraform state, state locking and consistency checking"
 }
+
+variable noncurrent_version_expiration {
+  type = "number"
+  description = "Days to expire non-current state versions"
+  default = 30
+}
